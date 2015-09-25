a2b194d8caaf23a5d17d18b62f875134b5ee3852
Ibrahim Sammour <s1mm0ur@outlook.com>
2015-09-25 17:35:41 +0300

    + General design

---
+++ ISammourAlert/Enums.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace ISammourAlert
+{
+    public enum AlertStyle
+    {
+        Normal,
+        Custom
+    }
+    public enum TextFieldStyle
+    {
+        AlphaNumeric,
+        Numeric
+    }
+}
